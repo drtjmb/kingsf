40801845da831d416837231156a1edaea3228468
Timothy Miles-Board <timothy.miles-board@london.ac.uk>
2014-12-27 14:13:27 +0000

    Merge all templates into pubs/templates/pubs

--- pubs/search_indexes.py
+++ pubs/search_indexes.py
@@ -2,7 +2,7 @@ from haystack import indexes;
 from pubs.models import Publication
 
 class PublicationIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True)
+    text = indexes.CharField(document=True, use_template=True, template_name='pubs/indexes/publication_text.txt')
     author = indexes.MultiValueField(model_attr='get_author_names', faceted=True)
     year = indexes.CharField(model_attr='year', faceted=True)
 
--- pubs/urls.py
+++ pubs/urls.py
@@ -7,6 +7,6 @@ from pubs import views
 sqs = SearchQuerySet().facet('year').facet('author')
 
 urlpatterns = patterns('haystack.views',
-    url(r'^search/', FacetedSearchView(form_class=FacetedSearchForm, searchqueryset=sqs), name='haystack_search'),
+    url(r'^search/', FacetedSearchView(form_class=FacetedSearchForm, searchqueryset=sqs, template='pubs/search.html'), name='haystack_search'),
     url(r'^(?P<pubid>\d+)/', views.detail, name='detail'),
 )
