9a848967c4f789a532c7b14740d62e102eda5427
Timothy Miles-Board <timothy.miles-board@london.ac.uk>
2014-12-27 12:24:34 +0000

    Add home and about pages

--- kingsf/urls.py
+++ kingsf/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'kingsf.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^about.html$', TemplateView.as_view(template_name='about.html')),
     url(r'^pubs/', include('pubs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
