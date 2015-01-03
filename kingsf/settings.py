e1852b2491c209eb7a82da7af12aa184705701ce
Timothy Miles-Board <timothy.miles-board@london.ac.uk>
2015-01-03 22:29:08 +0000

    Add mysql backend for models

--- kingsf/settings.py
+++ kingsf/settings.py
@@ -61,8 +61,10 @@ WSGI_APPLICATION = 'kingsf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'kingsf',
+        'USER': 'kingsf',
+        'PASSWORD': 'f9GAc3rk',
     }
 }
 
